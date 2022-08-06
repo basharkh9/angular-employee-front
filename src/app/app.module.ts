f23f9b1c24b7747114a904c895ae483a83d5081b
Bashar Khadra <bashar.work.92@gmail.com>
2022-08-06 23:05:09 +0400

    Initial Commit with working registeration and login funtionality.

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
